2715e1b09cc4ea8cf9d92c9326212b25890b8511
wip-abramson <will@truu.id>
2019-04-13 15:24:46 +0100

    add BurnTokensAction

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { CampaignHeader } from './lib'
 import { CommonsStateWrapper, MintTokensAction } from './lib'
 import UserComponent from './examples/UserComponent'
 import BurnTokensComponent from './examples/BurnTokensComponent';
-
+import {BurnTokensAction} from './lib/index'
 const App = () => (
   <div>
     <CommonsStateWrapper>
       <MintTokensAction buttonName="Mint tokens" fundingAmount={1000}/>
+      <BurnTokensAction buttonName="Burn tokens" fundingAmount={100}/>
       {/*<CampaignHeader />*/}
       {/*<UserComponent />*/}
     </CommonsStateWrapper>
--- src/lib/index.js
+++ src/lib/index.js
@@ -8,11 +8,13 @@ import { userActions } from './state/userState'
 import FormField from './stateless-components/FormField'
 import CampaignContext, { CampaignProvider } from './context/Campaign';
 import {campaignActions} from './state/campaignState';
+import BurnTokensAction from './components/BurnTokensAction';
 export { CommonsStateWrapper }
 export { CampaignHeader }
 export { MintTokensAction }
 export { UserContext, UserProvider, userActions }
 export { CampaignContext, CampaignProvider, campaignActions}
+export {BurnTokensAction}
 export { PrimaryButton, SecondaryButton }
 // Stateless components
 export { FormField }
