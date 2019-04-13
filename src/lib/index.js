f59aa3e041414b3a60aae7a3b6f53f88bdf7895e
wip-abramson <will@truu.id>
2019-04-13 16:24:46 +0100

    add shared commons nav header

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,13 +4,6 @@ import DaiAction from './components/DaiAction'
 import PrimaryButton from './stateless-components/PrimaryButton'
 import SecondaryButton from './stateless-components/SecondaryButton'
 import FormField from './stateless-components/FormField'
-import UserContext, { UserProvider } from './context/User'
-import { userActions } from './state/userState'
-import { getConviction } from './state/convictionlib'
-import AugmentedCurveContext, { AugmentedCurveProvider } from './context/AugmentedCurve'
-import ConvictionVotingCurveContext, { ConvictionVotingCurveProvider } from './context/ConvictionVotingCurve'
-import { curveActions } from './state/augmentedCurveState'
-import { cvActions } from './state/convictionVotingCurveState'
 import UserContext, {UserProvider,} from "./context/User";
 import {userActions} from "./state/userState"
 import {getConviction} from "./state/convictionlib"
@@ -23,15 +16,8 @@ export { CommonsStateWrapper }
 export { CampaignHeader }
 export { DaiAction }
 export { UserContext, UserProvider, userActions }
-<<<<<<< HEAD
-export { AugmentedCurveContext, AugmentedCurveProvider, curveActions }
-export { ConvictionVotingCurveContext, ConvictionVotingCurveProvider, cvActions }
-export { PrimaryButton, SecondaryButton, FormField }
-export { getConviction }
-=======
 export { AugmentedCurveContext, AugmentedCurveProvider, curveActions}
 export { ConvictionVotingCurveContext, ConvictionVotingCurveProvider, cvActions}
 export {PrimaryButton, SecondaryButton, FormField}
 export {getConviction}
 export {CommonsNavHeader}
->>>>>>> add shared commons nav header
